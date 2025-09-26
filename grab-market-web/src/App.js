bbbc152f0b1417ad22afd527925138a068fa2d26
PI11A203-キムスミン <ellan122316@gmail.com>
2025-09-26 16:12:45 +0900

    React로 웹 개발하기[1,2]

--- grab-market-web/src/App.js
+++ grab-market-web/src/App.js
@@ -1,29 +1,8 @@
-import logo from "./logo.svg";
 import "./App.css";
-import ChildComponent from "./child.js";
-import TimerComponent from "./timer.js";
+import MainPageComponent from "./main";
 
 function App() {
-  const text = "안녕하세요, 월드";
-  const sayHello = function () {
-    return <h3>연습 함수를 적어봤습니다</h3>;
-  };
-  const sayHello2 = function () {
-    alert("onClick 이벤트입니다");
-  };
-  return (
-    <div>
-      <h1>Hello, World!</h1>
-      <h2>{text}</h2>
-      {sayHello()}
-      <div onClick={sayHello2}>Click me</div>
-      <TimerComponent />
-      <ChildComponent name="수민" age={30} />
-      <ChildComponent name="재희" age={25} />
-      <ChildComponent name="민수" age={28} />
-      <ChildComponent name="정연" age={22} />
-    </div>
-  );
+  return <MainPageComponent />;
 }
 
 export default App;
