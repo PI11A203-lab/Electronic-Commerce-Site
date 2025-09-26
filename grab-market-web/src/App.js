c86033d8a01e1668c36d6841ae9a70de7ee03a5c
PI11A203-キムスミン <ellan122316@gmail.com>
2025-09-26 15:01:50 +0900

    props, state, useEffect

--- grab-market-web/src/App.js
+++ grab-market-web/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import ChildComponent from "./child.js";
+import TimerComponent from "./timer.js";
 
 function App() {
+  const text = "안녕하세요, 월드";
+  const sayHello = function () {
+    return <h3>연습 함수를 적어봤습니다</h3>;
+  };
+  const sayHello2 = function () {
+    alert("onClick 이벤트입니다");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello, World!</h1>
+      <h2>{text}</h2>
+      {sayHello()}
+      <div onClick={sayHello2}>Click me</div>
+      <TimerComponent />
+      <ChildComponent name="수민" age={30} />
+      <ChildComponent name="재희" age={25} />
+      <ChildComponent name="민수" age={28} />
+      <ChildComponent name="정연" age={22} />
     </div>
   );
 }
