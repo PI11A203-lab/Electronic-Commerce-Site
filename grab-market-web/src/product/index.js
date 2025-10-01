03f705cbe4c46bb8f65ad57bd0a472d3ef5e5109
PI11A203-キムスミン <ellan122316@gmail.com>
2025-10-01 16:57:38 +0900

    商品詳細ページの実装 - 1

--- grab-market-web/src/product/index.js
+++ grab-market-web/src/product/index.js
@@ -1,7 +1,23 @@
 import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function ProductPage() {
   const { id } = useParams();
+  const [product, setProduct] = useState(null);
+  useEffect(function () {
+    axios
+      .get(
+        `https://341cf269-c712-4751-a587-2c7fd1b972ec.mock.pstmn.io/products/${id}`
+      )
+      .then(function (result) {
+        setProduct(result.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
+  console.log(product);
   return <h1>Product Page {id} Product</h1>;
 }
 
