0471fb475cacb663603c3c3aaa8cfb59cb31bbe3
PI11A203-キムスミン <ellan122316@gmail.com>
2025-10-01 18:44:56 +0900

    商品詳細ページの実装 - 2

--- grab-market-web/src/product/index.js
+++ grab-market-web/src/product/index.js
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import "./index.css";
 
 function ProductPage() {
   const { id } = useParams();
@@ -17,8 +18,27 @@ function ProductPage() {
         console.error(error);
       });
   }, []);
-  console.log(product);
-  return <h1>Product Page {id} Product</h1>;
+
+  if (product === null) {
+    return <h1>상품 정보를 받고 있습니다...</h1>;
+  }
+
+  return (
+    <div>
+      <div id="image-box">
+        <img src={"/" + product.imageUrl} />
+      </div>
+      <div id="profile-box">
+        <img src="/images/icons/avatar.png" />
+        <span>{product.seller}</span>
+      </div>
+      <div id="contents-box">
+        <div id="name">{product.name}</div>
+        <div id="price">{product.price}원</div>
+        <div id="description">{product.description}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ProductPage;
