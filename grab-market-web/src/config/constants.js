9596ce578ce435aa95350e6106de08a4ee8b73db
kim020613 <ellan1223@naver.com>
2025-11-10 17:49:30 +0900

    API URL 住所変更

--- grab-market-web/src/config/constants.js
+++ grab-market-web/src/config/constants.js
@@ -1,1 +1,4 @@
-export const API_URL = "http://localhost:8081";
+export const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://grab-market-server-tokyo.fly.dev"
+    : "http://localhost:8081";
