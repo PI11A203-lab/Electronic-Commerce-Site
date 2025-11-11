6ebfdd92706d479de46cb8ee0e44683395c0a240
kim020613 <ellan1223@naver.com>
2025-11-11 14:55:02 +0900

    React Native

--- grab-market-mobile/App.js
+++ grab-market-mobile/App.js
@@ -1,11 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.box}>
+        <Text>Nested View 1</Text>
+      </View>
+      <View style={styles.box}>
+        <Text>Nested View 2</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +19,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: "blue",
+    marginBottom: 8,
   },
 });
